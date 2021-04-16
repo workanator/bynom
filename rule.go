ca23e98f5ca41cc388807bb76904cca4d111c920
workanator <workanator@gmail.com>
2021-04-16 15:23:18 +0500

    Document Rule

--- rule.go
+++ rule.go
@@ -1,15 +1,19 @@
 package bynom
 
+// Rule composes multiple parsing functions into complex parsing logic.
+// Rule implements Eater interface.
 type Rule struct {
 	noms []Nom
 }
 
+// NewRule creates a new Rule instance from the parsers noms.
 func NewRule(noms ...Nom) *Rule {
 	return &Rule{
 		noms: noms,
 	}
 }
 
+// Eat parses the next piece on the Plate p.
 func (rule *Rule) Eat(p Plate) (err error) {
 	var startPos int
 	if startPos, err = p.TellPosition(); err != nil {
