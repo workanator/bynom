e3e53ae6c32ea18321fb008b38b71754e2c5a4d7
workanator <workanator@gmail.com>
2021-04-16 14:38:28 +0500

    Add TakeFunc function

--- vars.go
+++ vars.go
@@ -38,3 +38,30 @@ func Take(dst *[]byte, noms ...Nom) Nom {
 		return
 	}
 }
+
+func TakeFunc(fn func([]byte) error, noms ...Nom) Nom {
+	return func(p Plate) (err error) {
+		var startPos int
+		if startPos, err = p.TellPosition(); err != nil {
+			return
+		}
+
+		for _, nom := range noms {
+			if err = nom(p); err != nil {
+				return
+			}
+		}
+
+		var endPos int
+		if endPos, err = p.TellPosition(); err != nil {
+			return
+		}
+
+		var s []byte
+		if s, err = p.ByteSlice(startPos, endPos); err != nil {
+			return
+		}
+
+		return fn(s)
+	}
+}
