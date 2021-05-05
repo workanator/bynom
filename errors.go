3eb511b1649744f85cf524d0a4c0128f89192dbf
workanator <workanator@gmail.com>
2021-05-05 12:07:40 +0500

    Move parse context to separate struct

--- errors.go
+++ errors.go
@@ -56,7 +56,7 @@ type ErrParseFailed struct {
 	Err         error
 	StartPos    int
 	EndPos      int
-	Context     []byte
+	Context     *ParseContext
 	Breadcrumbs []Breadcrumb
 }
 
@@ -65,10 +65,9 @@ func (e *ErrParseFailed) Error() string {
 	sb.WriteString(e.Err.Error())
 	sb.WriteString(fmt.Sprintf(", start position: %d, end position: %d", e.StartPos, e.EndPos))
 
-	if len(e.Context) > 0 {
-		sb.WriteString(", context: '")
-		_, _ = sb.Write(e.Context)
-		sb.WriteByte('\'')
+	if e.Context != nil {
+		sb.WriteString(", context: ")
+		sb.WriteString(e.Context.String())
 	}
 
 	if len(e.Breadcrumbs) > 0 {
@@ -91,45 +90,31 @@ func (e *ErrParseFailed) Unwrap() error {
 }
 
 func (e *ErrParseFailed) CopyContext(ctx context.Context, p Plate, startPos, endPos, chunkLen int) {
-	var ctxLen = endPos - startPos
+	e.Context = new(ParseContext)
+
+	var (
+		ctxLen = endPos - startPos
+		buf    []byte
+	)
 	if ctxLen <= chunkLen {
-		if buf, err := p.ByteSlice(ctx, startPos, endPos); err == nil {
-			e.Context = make([]byte, ctxLen)
-			copy(e.Context, buf)
+		if buf, e.Context.HeadErr = p.ByteSlice(ctx, startPos, endPos); e.Context.HeadErr == nil {
+			e.Context.Head = make([]byte, len(buf))
+			copy(e.Context.Head, buf)
 		}
 	} else {
-		var leftLen = ctxLen / 2
-		leftBuf, leftErr := p.ByteSlice(ctx, startPos, startPos+leftLen)
+		e.Context.Parted = true
+		e.Context.BytesRemain = ctxLen - chunkLen
 
-		var rightLen = ctxLen - leftLen
-		rightBuf, rightErr := p.ByteSlice(ctx, endPos-rightLen, endPos)
+		var leftLen = chunkLen / 2
+		if buf, e.Context.HeadErr = p.ByteSlice(ctx, startPos, startPos+leftLen); e.Context.HeadErr == nil {
+			e.Context.Head = make([]byte, len(buf))
+			copy(e.Context.Head, buf)
+		}
 
-		if leftErr == nil && rightErr == nil {
-			var (
-				bytesRemain = ctxLen - chunkLen
-				remainMsg   = []byte(fmt.Sprintf("..[%d bytes]..", bytesRemain))
-				msgLen      = len(remainMsg)
-			)
-			e.Context = make([]byte, ctxLen+msgLen)
-			copy(e.Context[:leftLen], leftBuf)
-			copy(e.Context[leftLen:leftLen+msgLen], remainMsg)
-			copy(e.Context[leftLen+msgLen:], rightBuf)
-		} else if leftErr == nil {
-			var (
-				remainMsg = []byte("..[more bytes]")
-				msgLen    = len(remainMsg)
-			)
-			e.Context = make([]byte, leftLen+msgLen)
-			copy(e.Context[:leftLen], leftBuf)
-			copy(e.Context[leftLen:leftLen+msgLen], remainMsg)
-		} else if rightErr == nil {
-			var (
-				remainMsg = []byte("[more bytes]..")
-				msgLen    = len(remainMsg)
-			)
-			e.Context = make([]byte, rightLen+msgLen)
-			copy(e.Context[:msgLen], remainMsg)
-			copy(e.Context[msgLen:], rightBuf)
+		var rightLen = chunkLen - leftLen
+		if buf, e.Context.TailErr = p.ByteSlice(ctx, endPos-rightLen, endPos); e.Context.TailErr == nil {
+			e.Context.Tail = make([]byte, len(buf))
+			copy(e.Context.Tail, buf)
 		}
 	}
 }
@@ -181,6 +166,55 @@ func ExtendBreadcrumb(err error, startPos, endPos int) error {
 	}
 
 	return err
+}
+
+type ParseContext struct {
+	Head        []byte
+	HeadErr     error
+	Tail        []byte
+	TailErr     error
+	BytesRemain int
+	Parted      bool
+}
+
+func (pc ParseContext) String() string {
+	var sb strings.Builder
+
+	if pc.Parted {
+		if pc.HeadErr == nil && pc.TailErr == nil {
+			sb.WriteByte('\'')
+			_, _ = sb.Write(pc.Head)
+			if pc.BytesRemain > 0 {
+				sb.WriteString("..[")
+				sb.WriteString(strconv.Itoa(pc.BytesRemain))
+				sb.WriteString(" bytes]..")
+			}
+			_, _ = sb.Write(pc.Tail)
+			sb.WriteByte('\'')
+		} else if pc.HeadErr == nil {
+			sb.WriteByte('\'')
+			_, _ = sb.Write(pc.Head)
+			sb.WriteString("..[more bytes]'")
+		} else if pc.TailErr == nil {
+			sb.WriteString("'[more bytes]..")
+			_, _ = sb.Write(pc.Tail)
+			sb.WriteByte('\'')
+		} else {
+			sb.WriteByte('!')
+			sb.WriteString(pc.HeadErr.Error())
+		}
+	} else {
+		if pc.HeadErr == nil {
+			sb.WriteByte('\'')
+			_, _ = sb.Write(pc.Head)
+			sb.WriteByte('\'')
+		} else {
+			sb.WriteByte('!')
+			sb.WriteString(pc.HeadErr.Error())
+		}
+	}
+
+	return sb.String()
 }
 
 type Breadcrumb struct {
